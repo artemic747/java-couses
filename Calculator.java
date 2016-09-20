b37657800abb91479e462005fe6d1f893d605e8f
Artemic <annapopova_2013@mail.ru>
2016-09-20 15:54:33 +0300

    user input

--- Calculator.java
+++ Calculator.java
@@ -1,22 +1,19 @@
 public class Calculator {
-	
 	private int result;
-	
-	public void add(int ... params){
-		for(Integer param : params){
-			this.result += param;
-		}
-	}
-	
+	public void add(int params, int params2, String operator){
+			if(operator.equals("+")){this.result = params + params2;}
+			else if(operator.equals("-")){this.result = params - params2;} 
+			else if(operator.equals("*")){this.result = params * params2;} 
+			else if(operator.equals("/")){this.result = params / params2;}
+			else if(operator.equals("^")){this.result = (int) Math.pow(params, params2);}
+				
+			}
 	
 	public int getResult(){
 		return this.result;
 	}
 	
-	
 	public void clearResult(){
 		this.result = 0;
 	}
-	
-	
 }
