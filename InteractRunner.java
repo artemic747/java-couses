80d7743d02e52ad30f3b71d52d2473f516cb90bb
Artemic <annapopova_2013@mail.ru>
2016-09-21 10:05:32 +0300

    user input

--- InteractRunner.java
+++ InteractRunner.java
@@ -1,58 +1,53 @@
 import java.util.Scanner;
 
 public class InteractRunner {
+	
 	public static void main(String[]args){
 		Scanner reader = new Scanner(System.in);
-
-try{
-    Calculator calc = new Calculator();
-        String exit = "no";
-        String UseResult = "no";
-        
-        	while (!exit.equals("yes"))
-                {
+		try{
+			Calculator calc = new Calculator();
+				String exit = "no";
+				String UseResult = "no";
+				while (!exit.equals("yes")){
                     String first;
                     if(UseResult.equals("yes")){
                         first = String.valueOf(calc.getResult());
                         System.out.println(first);
-                    } else 
-                    {
+						} else{
                         System.out.println("Enter first arg : ");
                         first = reader.next();
-                    }
+						}
                         
                         System.out.println("Enter operator");
                         String operator = reader.next();
                             while(!(operator.equals("^") || operator.equals("+") || operator.equals("/")|| operator.equals("-")|| operator.equals("*"))){
                             System.out.println("Enter operator");
                             operator = reader.next();
-                    }
+							}
                     
-                        System.out.println("Enter second arg : ");
-                        String second = reader.next();                                        
+							System.out.println("Enter second arg : ");
+							String second = reader.next();                                        
                             while(second.equals("0") && operator.equals("/")){
                             System.out.println("Division by zero");
                             System.out.println("Enter second arg : ");
                             second = reader.next();
-                    }
+							}
                     	
-                        System.out.println("Exit : yes/no ");
-                        exit = reader.next();
+							System.out.println("Exit : yes/no ");
+							exit = reader.next();
 
                             if(exit.equals("no")){
-                                calc.add(Integer.valueOf(first), Integer.valueOf(second), operator);
-                                System.out.println("Use result : yes/no ");
-                                UseResult = reader.next();
-                            } else calc.add(Integer.valueOf(first), Integer.valueOf(second), operator);
-                        }
-                
-        System.out.println("Result : " + calc.getResult());
-}
-                
-        finally 
-        {
-	reader.close();
+                            calc.add(Integer.valueOf(first), Integer.valueOf(second), operator);
+                            System.out.println("Use result : yes/no ");
+                            UseResult = reader.next();
+                            } 
+							else { 
+							calc.add(Integer.valueOf(first), Integer.valueOf(second), operator);
+							}
+						}
+                System.out.println("Result : " + calc.getResult());
+		}finally{
+			reader.close();
+		}
 	}
-	
-    }
 }
