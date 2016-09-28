470ae0a5ef29b8b796b2cf7957edd577e05398d5
Artemic <annapopova_2013@mail.ru>
2016-09-28 23:42:39 +0300

    ustr input

--- InteractRunner.java
+++ InteractRunner.java
@@ -6,33 +6,41 @@ public class InteractRunner {
 		Scanner reader = new Scanner(System.in);
 		try{
 			Calculator calc = new Calculator();
-			
-			//exit - выход из программы
+			/**
+			 exit - выход из программы
+			*/
 			String exit = "no";
-			
-			//UseResult - использование результата в дальнейшем вычислении
+			/**
+			 UseResult - использование результата в дальнейшем вычислении
+			*/			
 			String UseResult = "no";
 				
 				while (!exit.equals("yes")){
                     String first;
-			
-					// Ввод первого операнда. 
-					if(UseResult.equals("yes")){
+					/**
+					Ввод первого операнда. 
+					*/
+                    if(UseResult.equals("yes")){
                         first = String.valueOf(calc.getResult());
+                        System.out.println(first);
 						} else{
                         System.out.println("Enter first arg : ");
                         first = reader.next();
 						}
                         
-					// Ввод оператора. 
-					System.out.println("Enter operator");
+					/**
+					Ввод оператора. 
+					*/
+                    System.out.println("Enter operator");
                     String operator = reader.next();
                         while(!(operator.equals("^") || operator.equals("+") || operator.equals("/")|| operator.equals("-")|| operator.equals("*"))){
                         System.out.println("Enter operator");
                         operator = reader.next();
 						}
                     
-					//Ввод второго операнда с проверкой деления на ноль.
+					/**
+					Ввод второго операнда с проверкой деления на ноль.
+					*/
 					System.out.println("Enter second arg : ");
 					String second = reader.next();                                        
                         while(second.equals("0") && operator.equals("/")){
@@ -57,8 +65,9 @@ public class InteractRunner {
 						calc.add(Integer.valueOf(first), Integer.valueOf(second), operator);
 						}
 					}
-					
-				//итоговый результат
+					/**
+					итоговый результат
+					*/
                 System.out.println("Result : " + calc.getResult());
 		}finally{
 			reader.close();
