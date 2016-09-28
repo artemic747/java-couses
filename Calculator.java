9b9272a1beb3267e188f0cb839dac5b2029377cb
Artemic <annapopova_2013@mail.ru>
2016-09-28 19:27:33 +0300

    ustr input

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,7 @@
 public class Calculator {
 	private int result;
+	
+		//Метод проводит математические операции 
 	public void add(int params, int params2, String operator){
 			if(operator.equals("+")){this.result = params + params2;}
 			else if(operator.equals("-")){this.result = params - params2;} 
@@ -8,11 +10,12 @@ public class Calculator {
 			else if(operator.equals("^")){this.result = (int) Math.pow(params, params2);}
 				
 			}
-	
+	//Метод возвращает результат 
 	public int getResult(){
 		return this.result;
 	}
 	
+	//Метод очищает результат
 	public void clearResult(){
 		this.result = 0;
 	}
